5ed436fbd906889e7e96620b761b84d9b93b91a7
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2025-03-03 23:44:19 +0700

    forgot to add document request - resqponse

--- app/models/document.py
+++ app/models/document.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel, Field
+from typing import List
 
 class Document(BaseModel):
     file_path: str
     file_type: str = Field(..., description="Supported types: pdf, csv, txt")
+
+class DocumentsUploadRequest(BaseModel):
+    documents: List[Document]
+
